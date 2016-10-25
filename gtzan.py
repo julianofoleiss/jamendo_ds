dfdb6ef1aa5a45a54c3c935866f09b0cdc091e04
Juliano Henrique Foleiss <julianofoleiss@utfpr.edu.br>
2016-10-25 16:12:22 -0200

    renamed gtzan.py to cli.py

--- gtzan.py
+++
@@ -1,9 +0,0 @@
-import jamendo_ds as jd
-import sys
-
-if __name__ == "__main__":
-
-    jd.with_genres( ["blues", "classical", "country", "disco", "hiphop", "jazz", "metal", "pop", "reggae", "rock"] )
-
-    
-
