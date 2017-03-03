55ed4c68878cc397a31af42a6c903891c9c48369
Juliano <julianofoleiss@utfpr.edu.br>
2017-03-03 18:39:25 -0300

    added option to pass target rate to downsample_songs.py

--- downsample_songs.py
+++ downsample_songs.py
@@ -18,7 +18,7 @@ def trim_middle(infile, outfile, position, duration):
 
     subprocess.call(command)
 
-def downsample(infile, outfile, mix):
+def downsample(infile, outfile, mix, rate):
 
     command = "sox %s %s " % (infile, outfile)
 
@@ -26,9 +26,9 @@ def downsample(infile, outfile, mix):
         command += "channels 1 "
 
     for i in range(6):
-        command += "lowpass 11025 "
+        command += "lowpass %d " % ( int(rate / 2))
     
-    command += "rate 22050 "
+    command += "rate %d " % (rate)
 
     subprocess.call(command, shell=True)
 
@@ -37,15 +37,16 @@ def downsample_thread(work):
     song = work[0]
     path = work[1]
     mix = work[2]
+    rate = work[3]
+    print("Downsampling %s to %dHz..." % (path, rate))
 
-    print("Downsampling %s to 22050KHz..." % path)
-
-    downsample(song, path, mix)
+    downsample(song, path, mix, rate)
 
 if __name__ == "__main__":
 
     mp3folder = sys.argv[1]
     out_folder = sys.argv[2]
+    rate = int(sys.argv[3])
 
     out_format = "wav" if "towav" in sys.argv else "mp3"
     mix_channels = True if "mixchannels" in sys.argv else False
@@ -64,7 +65,7 @@ if __name__ == "__main__":
 
     for song in songs:
         filename = os.path.splitext(song)[0].split("/")[-1]
-        work.append((song, out_folder + filename + "_22KHz." + out_format, mix_channels))
+        work.append((song, out_folder + filename + "_%dHz." % (rate) + out_format, mix_channels, rate))
     
     pool.map(downsample_thread, work)
     
